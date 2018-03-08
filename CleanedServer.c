efbb05615f21ca83e5bbae80f22ce1c1b4a6089d
cerdelyi <35412075+cerdelyi@users.noreply.github.com>
2018-03-08 00:48:31 -0800

    Add files via upload

---
+++ CleanedServer.c
@@ -0,0 +1,294 @@
+//
+//  CleanedServer.c
+//  CS537_TCP_Server
+//
+//  Created by Christopher Erdelyi on 3/7/18.
+//  Copyright © 2018 Christopher Erdelyi. All rights reserved.
+//
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAXLINE 2048
+#define CHUNK 1
+
+const int backlog = 4;
+
+
+void *clientHandler(void *arg)
+{
+    
+    char str[MAXLINE];
+    
+    int n;
+    
+    
+    const char* data =
+    "HTTP/1.1 200 OK\n"
+    "Content-Type: text/html\n"
+    "Accept-Ranges: bytes\n"
+    "Content-Length: ";
+    
+    const char* jpgHeader =
+    "HTTP/1.1 200 OK\n"
+    "Content-Type: image/jpeg\n"
+    "Accept-Ranges: bytes\n"
+    "Content-Length: ";
+    
+    const char *trailingNewline = "\r\n\r\n";
+    
+ //   char* buffer;
+    size_t size = 10;
+    
+    //    int testLength = msgLength(data1);
+    
+    int fd = *(int*)(arg);
+    char* string_tokens;
+    //  char* getCommand = "GET";
+    
+  //  FILE* testHTML = fopen("test.html", "r");
+    
+    // char inputString[999];
+    
+    
+   /* int ch;
+    size_t len = 0;
+    buffer = realloc(NULL, sizeof(char)*size);//size is start size
+    
+    
+    
+    while(EOF!=(ch=fgetc(testHTML))){
+        buffer[len++]=ch;
+        if(len==size){
+            buffer = realloc(buffer, sizeof(char)*(size+=16));
+        }
+    }
+    buffer[len++]='\0';
+    
+    
+    buffer = realloc(buffer, sizeof(char)*len);
+    
+    fclose(testHTML);
+    */
+    
+    ////////////////////////////////////////////////
+    // IMAGE HANDLING SECTION
+    
+    /*
+     FILE* testGif = fopen("indyicon.jpg", "r");
+     int gifsize;
+     fseek(testGif, 0, SEEK_END);
+     gifsize = ftell(testGif);
+     fseek(testGif, 0, SEEK_SET);
+     
+     char* fullGifHeader = (char*) malloc(10+strlen(gifHeader)+gifsize);
+     char* gifContentSize= (char*) malloc(gifsize);
+     
+     snprintf(gifContentSize, gifsize, "%d", gifsize);
+     strcpy(fullGifHeader, gifHeader);
+     strcat(fullGifHeader, gifContentSize);
+     strcat(fullGifHeader, trailingNewline);
+     
+     //Send Picture as Byte Array
+     
+     char send_buffer[1];
+     while(!feof(testGif)) {
+     fread(send_buffer, 1, sizeof(send_buffer), testGif);
+     strcat(fullGifHeader, send_buffer);
+     //write(fd, send_buffer, sizeof(send_buffer));
+     bzero(send_buffer, sizeof(send_buffer));
+     } */
+
+    
+    while (1) {
+        
+        if ((n = read(fd, str, MAXLINE)) == 0) {
+            write(fd, "closing connection", MAXLINE);
+            close (fd);
+            return 0;
+        }
+        
+        
+        string_tokens= strtok(str, " ");
+        if (strncmp(string_tokens, "GET", 3)==0){
+            
+          
+            string_tokens= strtok(NULL, " ");
+            
+                //Check for HTML file request
+                if(strncmp(string_tokens, "/", 3)==0)    //Default case. Browser is asking for index.html
+                {
+                    
+                    char* fileToOpen = (char*) malloc(strlen(string_tokens));
+                    
+                    strcpy(fileToOpen, string_tokens);
+                   
+                    if(strncmp(fileToOpen, "/",1)==0)
+                    {
+                        strcpy(fileToOpen, "index.html");
+                    }
+                  
+                    FILE* file = fopen(fileToOpen, "r");
+                    fseek(file, 0, SEEK_END);
+                    int fileLen=ftell(file);
+                    char file_data[fileLen];
+                    rewind(file);
+                    
+                    fread(file_data, sizeof(char), fileLen, file);
+    
+                    int ContentHeaderSize = fileLen;
+                    char Content_Header_Length[ContentHeaderSize];
+                    char* fullHeader = (char*) malloc(strlen(trailingNewline)+ strlen(data)+strlen(Content_Header_Length));
+                    char* fullData = (char*) malloc(strlen(fullHeader)+ strlen(file_data));
+                    
+                    snprintf(Content_Header_Length, ContentHeaderSize, "%d", ContentHeaderSize);
+                    
+                    strcpy(fullHeader, data);
+                    strcat(fullHeader, Content_Header_Length);
+                    strcat(fullHeader, trailingNewline);
+                    strcpy(fullData, fullHeader);
+                    strcat(fullData, file_data);
+                    
+                    write(fd, fullData, strlen(fullData)+1);
+                }
+            
+            
+                //Check for image file request
+                if (strncmp(string_tokens, "/indyicon.jpg", 12)==0)
+                {
+                    char* fileToOpen = (char*) malloc(strlen(string_tokens));
+                    strcpy(fileToOpen, "indyicon.jpg");
+                    
+                    FILE* file = fopen(fileToOpen, "rb");
+                    fseek(file, 0, SEEK_END);
+                    int fileLen=ftell(file);
+                    char file_data[fileLen];
+                    rewind(file);
+                    
+                    fread(file_data, sizeof(char), fileLen+1, file);
+                    
+                    //CONVERT LENGTH OF IMAGE FILE TO TEXT FORMAT FOR HEADER
+                    char Img_Content_Header_Length[fileLen];
+                    sprintf(Img_Content_Header_Length, "%d", fileLen);
+                    
+                    //PUT IT ALL TOGETHER
+                    char* fullImgHeader = malloc(strlen(trailingNewline) + strlen(Img_Content_Header_Length) + strlen(jpgHeader));
+                    strcpy(fullImgHeader, jpgHeader);
+                    strcat(fullImgHeader, Img_Content_Header_Length);
+                    strcat(fullImgHeader, trailingNewline);
+                    // strcat(fullImgHeader, file_data);
+                    
+                    //printf("file contents: %s", file_data);
+                    fclose(file);
+                    
+                    write(fd, fullImgHeader, strlen(fullImgHeader));
+                    write(fd, file_data, fileLen);
+                }
+            
+        }
+        
+        if(strncmp(string_tokens, "PUT", 3))
+        {
+            
+        }
+        
+        if(strncmp(string_tokens, "HEAD", 4))
+        {
+            
+            
+        }
+        
+        if(strncmp(string_tokens, "DELETE", 6))
+        {
+            
+            
+        }
+        
+        else {      // SEND 404 ERROR
+            write(fd, "404 error", MAXLINE);
+        }
+        
+        
+        //    free(buffer);
+        //    free(fullHeader);
+        //    free(fullData);
+        
+        
+    }
+    
+    
+}
+
+int main(int argc, char *argv[])
+{
+    
+    int    listenfd, connfd;
+    pthread_t tid;
+    int     clilen;
+    struct     sockaddr_in cliaddr, servaddr;
+    
+    if (argc != 3) {
+        printf("Usage: caseServer <address> <port> \n");
+        return -1;
+    }
+    
+    
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd == -1)
+    {
+        fprintf(stderr, "Error unable to create socket, errno = %d (%s) \n",
+                errno, strerror(errno));
+        return -1;
+    }
+    
+    bzero(&servaddr, sizeof(servaddr));
+    
+    servaddr.sin_family        = AF_INET;
+    servaddr.sin_addr.s_addr   = inet_addr(argv[1]);
+    servaddr.sin_port          = htons(atoi(argv[2]));
+    
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+        fprintf(stderr, "Error binding to socket, errno = %d (%s) \n",
+                errno, strerror(errno));
+        return -1;
+        
+    }
+    
+    if (listen(listenfd, backlog) == -1) {
+        fprintf(stderr, "Error listening for connection request, errno = %d (%s) \n",
+                errno, strerror(errno));
+        return -1;
+    }
+    
+    
+    while (1) {
+        clilen = sizeof(cliaddr);
+        if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0 ) {
+            if (errno == EINTR)
+                continue;
+            else {
+                fprintf(stderr, "Error connection request refused, errno = %d (%s) \n",
+                        errno, strerror(errno));
+            }
+        }
+        
+        if (pthread_create(&tid, NULL, clientHandler, (void *)&connfd) != 0) {
+            fprintf(stderr, "Error unable to create thread, errno = %d (%s) \n",
+                    errno, strerror(errno));
+        }
+        
+    }
+}
+
